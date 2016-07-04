2a8413d91b0767d570a10e35af7fe91a21786b0d
Kan Adachi <Kadachi1023@gmail.com>
2016-07-03 18:21:36 -0700

    [setup] added socket.io testing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const common = require('./common');
 const server = common.server;
 const app = common.app;
-const port = 3456;
+const io = common.io;
+const port = process.env.PORT || 3456;
+
+io.on('connection', (socket) => {
+
+})
 
 server.listen(port);
 console.log('listening on port 3456');
---
+++ spec/socket-endpoints/basic-spec.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const request = require('request');
+const app = require('../../server/server.js');
+const io = require('socket.io-client');
+
+const baseUrl = "http://localhost:3456/";
+let options = {
+  'transports': ['websocket'],
+  'force new connection': true
+}
+let client1, client2, client3;
+
+describe("socket.io connection", () => {
+  let clients = 0;
+
+  it("allows a client to connect", (done) => {
+    client1 = io.connect(baseUrl, options);
+    client1.on('connect', () => {
+      clients++;
+      done();
+    })
+  })
+
+  it ("allows multiple clients to connect", (done) => {
+    client2 = io.connect(baseUrl, options);
+    client3 = io.connect(baseUrl, options);
+    client2.on('connect', () => {
+      clients++;
+    });
+    client3.on('connect', () => {
+      clients++;
+      expect(clients).toBe(3);
+      done();
+      setTimeout(process.exit, 1000);
+    })
+  })
+});
+
