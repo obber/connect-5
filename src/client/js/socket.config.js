8e75b823cf819c032bcd2db0a93616720c9247ad
Kan Adachi <Kadachi1023@gmail.com>
2016-08-08 14:24:46 -0700

    [fix] added production url

--- src/client/js/socket.config.js
+++ src/client/js/socket.config.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const baseUrl = process.env.NODE_ENV === "production" ? "http://URLTOEDIT.com": "http://localhost:3456/";
+const baseUrl = process.env.NODE_ENV === "production" ? "http://http://ec2-52-38-24-188.us-west-2.compute.amazonaws.com:3456/": "http://localhost:3456/";
 
 let socket;
 
