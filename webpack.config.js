9147a8cfaedfdba08580c04d353cd1b4bc296dc8
Kan Adachi <Kadachi1023@gmail.com>
2016-08-08 14:46:48 -0700

    [fix] added production vars

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,11 @@ module.exports = {
         }
       ]
     },
-    exclude: "/node_modules/"
+    exclude: "/node_modules/",
+    plugins: [
+      new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      })
+    ]
   }
 }
