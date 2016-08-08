baea5b0f9d55412d8e2ad9cdeead7a50968f0795
Kan Adachi <Kadachi1023@gmail.com>
2016-08-08 15:08:37 -0700

    [fix] prod socket config val added

--- src/client/js/socket.config.js
+++ src/client/js/socket.config.js
@@ -1,6 +1,9 @@
 import io from "socket.io-client";
 
-const baseUrl = process.env.NODE_ENV === "production" ? "http://http://ec2-52-38-24-188.us-west-2.compute.amazonaws.com:3456/": "http://localhost:3456/";
+const baseUrl = process.env.NODE_ENV === "production" ? "http://ec2-52-38-24-188.us-west-2.compute.amazonaws.com:3456/": "http://localhost:3456/";
+
+console.log("baseUrl = ", baseUrl);
+console.log("process.env.NODE_ENV = ", process.env.NODE_ENV);
 
 let socket;
 
